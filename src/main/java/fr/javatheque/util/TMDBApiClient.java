be11522504bb18ad25a52e4b6a47d036a2522acc
Corentin <dup.corentin51@gmail.com>
2024-06-01 17:26:15 +0200

    fix tmdb

--- src/main/java/fr/javatheque/util/TMDBApiClient.java
+++ src/main/java/fr/javatheque/util/TMDBApiClient.java
@@ -2,56 +2,59 @@ package fr.javatheque.utils;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 public class TMDBApiClient {
     private static final String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
-    private static final String TMDB_API_KEY = "YOUR_API_KEY";
+    private static final String TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWE0ZmYwOTNiZmMzMDU3NmExODJjOTExOWE3NGJlOCIsInN1YiI6IjY1OGRlYTk5YTMzNjEyNTk5MDU5NjM5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z0foqNQE3_wbsEuVYquq0MB_5H_zngBJ2t9VilLk6oQ";
 
-    private final HttpClient httpClient;
+    private static TMDBApiClient instance;
+    private final OkHttpClient httpClient;
 
-    public TMDBApiClient() {
-        this.httpClient = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_2)
-                .build();
+    private TMDBApiClient() {
+        this.httpClient = new OkHttpClient();
     }
 
-    public String searchMovies(String query, String language, int page) throws IOException, InterruptedException {
-        String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
-        String url = TMDB_API_BASE_URL + "/search/movie?api_key=" + TMDB_API_KEY +
-                "&query=" + encodedQuery +
+    public static synchronized TMDBApiClient getInstance() {
+        if (instance == null) {
+            instance = new TMDBApiClient();
+        }
+        return instance;
+    }
+
+    public String searchMovies(String title, String language, int page) throws IOException {
+        String encodedQuery = URLEncoder.encode(title, StandardCharsets.UTF_8);
+        String url = TMDB_API_BASE_URL + "/search/movie?query=" + title +
                 "&language=" + language +
                 "&page=" + page;
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer " + TMDB_API_KEY)
                 .build();
+        Response response = httpClient.newCall(request).execute();
+        return response.body().string();
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        return handleResponse(response);
     }
 
-    public String getMovieDetails(int movieId, String language) throws IOException, InterruptedException {
-        String url = TMDB_API_BASE_URL + "/movie/" + movieId + "?api_key=" + TMDB_API_KEY +
-                "&language=" + language;
+    public String getMovieDetails(int movieId, String language) throws IOException {
+        String url = TMDB_API_BASE_URL + "/movie/" + movieId +
+                "?language=" + language;
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer " + TMDB_API_KEY)
                 .build();
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        return handleResponse(response);
-    }
-
-    private String handleResponse(HttpResponse<String> response) throws IOException {
-        if (response.statusCode() == 200) {
-            return response.body();
-        } else {
-            throw new IOException("Erreur lors de la requête API: " + response.statusCode());
-        }
+        Response response = httpClient.newCall(request).execute();
+        return response.body().string();
     }
 }
